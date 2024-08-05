e559253593cae335b781d86f8ca58fe75731807a
Chris Johnson <chris.johnson@senecacollege.ca>
2024-08-04 20:32:54 -0400

    Initial upload of Lab 10, Activity 1.

---
+++ lab10_activity1.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Title: lab10_activity1.bash
+# Student Name: <ENTER NAME>
+# Student E-mail: <ENTER SENECA E-MAIL ADDRESS>
+# Student ID Number: <ENTER STUDENT ID NUMBER>
+
+# Clears the screen at runtime
+clear
+
+# Script header code
+echo "UNX510 Summer 2024 - Lab 10, Activity 1"
+echo "---------------------------------------"
+
+# Task 1: Static calculations
+# Create a script which:
+# 1. Asks the user to enter 6 cities, one at a time, and store them into an array called: city_name
+# 1a. City names only. Do not include province/state, country, etc. May be helpful to include that in the user instuctions on screen (optional)
+# 1b. Some cities use more than one word. Your script should be able to handle this *without* it creating two different array items.
+# 1b Example: "New York" is one array item, not two.
+# 1c. Very helpful to let the user know what number they're curretly on. Example: "Please enter city name 5: "
+#
+# 2. Display array elements in reverse order in the output after all city names have been entered by the user.
+
+echo "| Task 1 |"
+
+# Begin code:
