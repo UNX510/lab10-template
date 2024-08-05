bf67e63947a26b4cbeeb6b796e5a2fd511805e13
Chris Johnson <chris.johnson@senecacollege.ca>
2024-08-04 21:00:55 -0400

    Updates Act1, creates Act2.

--- lab10_activity1.bash
+++ lab10_activity1.bash
@@ -12,16 +12,26 @@ clear
 echo "UNX510 Summer 2024 - Lab 10, Activity 1"
 echo "---------------------------------------"
 
-# Task 1: Static calculations
+# Task 1: Initialize and populate array with on-screen user input.
 # Create a script which:
 # 1. Asks the user to enter 6 cities, one at a time, and store them into an array called: city_name
 # 1a. City names only. Do not include province/state, country, etc. May be helpful to include that in the user instuctions on screen (optional)
 # 1b. Some cities use more than one word. Your script should be able to handle this *without* it creating two different array items.
 # 1b Example: "New York" is one array item, not two.
 # 1c. Very helpful to let the user know what number they're curretly on. Example: "Please enter city name 5: "
-#
-# 2. Display array elements in reverse order in the output after all city names have been entered by the user.
 
 echo "| Task 1 |"
+
+# Begin code:
+
+
+
+# Task 2: Display array elements.
+# Create a script which:
+# 2. Displays the array elements from *city_name* in reverse order on screen.
+# 2a. In your output iteration, be clear to note which array item number is being used, along with the actual value. This should start with array item 6 and say so.
+# 2a Example: "Array Item 6: New York" (or, alternatively, "The 6th city you entered was: New York")
+
+echo "| Task 2 |"
 
 # Begin code:
